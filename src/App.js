b33cd78548d7a78999312236402427947f66edc4
Nhat Hoang <hoangnh092185@gmail.com>
2017-05-08 14:03:25 -0700

    initial commit.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+var App = React.createClass({
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+});
+
+export default App;
