4243928c396027148d8e14c342b72f15fd12ced9
Nhat Hoang <hoangnh092185@gmail.com>
2017-05-08 16:35:09 -0700

    add profile and bio content.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+console.log(Component);
 
-var App = React.createClass({
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        name: 'Nhat Hoang',
+        biography: '31 year old Designer / Developer living in Portland. Love to make stuff.',
+      },
+      image: 'src="../public/IMG_0206.JPG"',
+      quote: {
+        content: 'A bed is a great place to sleep, but the best place is on the laptop while daddy is working.',
+        source: 'The Secret Life of Nemo, 13 year old dlh cat.'
+      }
+
+    };
+  }
   render() {
+      return(
+        <div className="App">
+          <Image src={this.state.image} />
+          <Profile person={this.state.person} quote={this.state.quote} />
+        </div>
+      );
+    }
+  }
+
+function Image(props){
     return (
-      <div className="App">
-        
+      <div className="Image" style={{backgroundImage: 'props.src'}}></div>
+    );
+}
+function Profile(props){
+  return (
+      <div className="Profile">
+        <h1 className="Name">{props.person.name}</h1>
+        <p className="Bio">{props.person.biography}</p>
+        <div className="Quote">
+          <blockquote>&ldquo; {props.quote.content} &rdquo;</blockquote>
+          <div className="byline">&mdash; {props.quote.source}</div>
+        </div>
+
       </div>
     );
-  }
-});
-
+}
 export default App;
